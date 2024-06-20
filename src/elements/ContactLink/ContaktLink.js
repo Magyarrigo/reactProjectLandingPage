a8e6a4c8f1974f9100acf49f3cfa2e46e0ef2da5
Bogus Drozd <b.drozd07@gmail.com>
2024-06-20 17:41:05 +0200

    ContactLink correction

--- src/elements/ContactLink/ContaktLink.js
+++ src/elements/ContactLink/ContaktLink.js
@@ -3,7 +3,7 @@ import "./ContactLink.css";
 function ContactLink({ text }) {
   return (
     <li className="navElement">
-      <a className="navLink contactButton" href="">
+      <a className="navLink contactButton" href="#">
         {text}
       </a>
     </li>
