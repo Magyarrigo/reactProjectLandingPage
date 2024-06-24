76a3d99729e48d2c190f3b1e7955524300df6d37
Bogus Drozd <b.drozd07@gmail.com>
2024-06-24 20:08:07 +0200

    data folder created

---
+++ src/data/offerData.js
@@ -0,0 +1,35 @@
+const offers = [
+  {
+    offerName: "audi 4x4",
+    isNew: false,
+  },
+  {
+    offerName: "BMW",
+    isNew: false,
+  },
+  {
+    offerName: "kia",
+    isNew: true,
+  },
+  {
+    offerName: "toyota",
+    isNew: false,
+  },
+  {
+    offerName: "subaru",
+    isNew: false,
+  },
+  {
+    offerName: "tesla",
+    isNew: true,
+  },
+  {
+    offerName: "new cars",
+    isNew: true,
+  },
+  {
+    offerName: "FIAT",
+    isNew: true,
+  },
+];
+export default offers;
--- src/elements/Offer/Offer.js
+++ src/elements/Offer/Offer.js
@@ -1,41 +1,7 @@
 import "./Offer.css";
 import Container from "../Container/Container";
 import OfferItem from "./OfferItem/OfferItem";
-
-const offers = [
-  {
-    offerName: "audi 4x4",
-    isNew: false,
-  },
-  {
-    offerName: "BMW",
-    isNew: false,
-  },
-  {
-    offerName: "kia",
-    isNew: true,
-  },
-  {
-    offerName: "toyota",
-    isNew: false,
-  },
-  {
-    offerName: "subaru",
-    isNew: false,
-  },
-  {
-    offerName: "tesla",
-    isNew: true,
-  },
-  {
-    offerName: "new cars",
-    isNew: true,
-  },
-  {
-    offerName: "FIAT",
-    isNew: true,
-  },
-];
+import offers from "../../data/offerData";
 
 function Offer({ offerTitle }) {
   return (
