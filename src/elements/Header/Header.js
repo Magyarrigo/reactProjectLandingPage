32a37a5c3faf390995a49324fce74aa24fcf967c
Bogus Drozd <b.drozd07@gmail.com>
2024-06-27 20:32:30 +0200

    header correction

--- src/elements/Header/Header.js
+++ src/elements/Header/Header.js
@@ -6,20 +6,22 @@ import Link from "./Link/Link";
 
 function Header() {
   return (
-    <Container>
-      <header className="header">
-        <div className="headerContainer">
-          <Logo textFirst="moja" textSecond="firma" />
-          <nav>
-            <ul className="headerNav">
-              <Link id="#aboutUs" text="o nas" />
-              <Link id="#ourOffer" text="oferta" />
-              <ContactLink text="kontakt" />
-            </ul>
-          </nav>
-        </div>
-      </header>
-    </Container>
+    <div className="header">
+      <Container>
+        <header className="header">
+          <div className="headerContainer">
+            <Logo textFirst="moja" textSecond="firma" />
+            <nav>
+              <ul className="headerNav">
+                <Link id="#aboutUs" text="o nas" />
+                <Link id="#ourOffer" text="oferta" />
+                <ContactLink text="kontakt" />
+              </ul>
+            </nav>
+          </div>
+        </header>
+      </Container>
+    </div>
   );
 }
 export default Header;
